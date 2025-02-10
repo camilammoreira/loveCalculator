9fa3591814a483bed402b8b17ab8b06936fb23d1
camila.moreira <camilammoreira1@gmail.com>
2025-02-09 22:59:27 -0300

    hide form after calculate

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 let result = Math.floor(Math.random() * 101);
+const subtitle = document.querySelector("#subtitle");
+const name1 = document.querySelector("#name1");
+const name2 = document.querySelector("#name2");
+const calcButton = document.querySelector("#calculate");
+const resultText = document.querySelector("#result");
+const forms = document.querySelectorAll(".form");
+const refresh = document.querySelector("#refresh")
 
-const name1 = document.getElementById("name1").value;
-const name2 = document.getElementById("name2").value;
-const calcButton = document.getElementById("calculate");
-const resultText = document.getElementById("result");
-
-calcButton.addEventListener("click", ()=> {
+calcButton.addEventListener("click", (event)=> {
     event.preventDefault();
     resultText.innerHTML = result + "%";
+    resultText.style.display = "block";
+    forms.forEach(element => {
+        element.style.display = "none";
+    });
+    subtitle.innerHTML = name1.value + " and " + name2.value + ", your compatibility is...";
+    refresh.style.display = "block";
+});
+
+refresh.addEventListener("click", ()=>{
+    location.reload();
 });
